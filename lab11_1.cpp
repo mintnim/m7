de95a841eead72c953c4c32d411a29e15fde40ac
mintnim <minttotk@gmail.com>
2021-01-15 17:04:55 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(long double x);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+	cout << fibonacci(50);
     return 0; 
 }
+
+long long int fibonacci(long double x){
+    if(x == 0){
+        return 0;
+    }
+    else if(x == 1){
+        return 1;
+    }
+    else if(x > 1){
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+    else{
+    	return 0;
+	}
+}
+
+
+
--- lab11_4.cpp
+++ lab11_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename Mm>
+void mySwap(Mm &,Mm &); 
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename Mm>
+void mySwap(Mm &Xx,Mm &Yy){
+    Mm r = Yy;
+    Yy = Xx;
+    Xx = r;
 }
 
